df643d8a615d201751451032b9c3598c3f19c69a
Iaroslav Pyroogv <ipyrogov@mindk.com>
2024-05-18 15:01:32 +0700

    🐛 (config.py): update model name from "sd3" to "sd3-turbo" for better clarity 🐛 (config.py): increase the number of steps from 50 to 150 for better image generation quality

--- slack_openai_response/config.py
+++ slack_openai_response/config.py
@@ -20,13 +20,13 @@ config = {
     "diffusion_image_generation": {
         "initial_comment": "Ось згенероване Stable Diffusion зображення:",
         "endpoint": "https://api.stability.ai/v2beta/stable-image/generate/sd3",
-        "model": "sd3",
+        "model": "sd3-turbo",
         "cfg_scale": 7,
         "clip_guidance_preset": "FAST_BLUE",
         "height": 512,
         "width": 512,
         "samples": 1,
-        "steps": 50,
+        "steps": 150,
         "output_format": "jpeg"
     },
     "base_prompt": (
